5d3ca1c52b47510378e926fb10579dbb74549f50
孔尚杰 <kongsj@zz91.net>
2012-04-23 16:44:19 +0800

    完成:新建smsutil工具类

---
+++ zz91-utils/src/main/java/com/zz91/util/sms/SmsUtil.java
@@ -0,0 +1,14 @@
+package com.zz91.util.sms;
+
+public class SmsUtil {
+
+	private static String API_HOST="http://apps.zz91.com/zz91-sms/";
+	private static SmsUtil _instance;
+	public void send(){
+		
+	}
+	
+	public static void main(){
+//		SmsUtil.getInstance().send("13587871118","你好阿")
+	}
+}
